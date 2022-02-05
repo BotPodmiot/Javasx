b65ecf19811fac2c0ab9ad15b4ab34f193ddb0bb
BotPodmiot <Pandek.xyz@gmail.com>
2022-02-05 03:48:10 +0100

    operatoryLog

---
+++ operatoryLog.java
@@ -0,0 +1,11 @@
+import javax.siwng.JOptionPane; 
+ 
+public class operatoryLog
+ {
+
+    public static void main(String args[]) {
+        
+        String userName = JOptionPane.showInputDialog("User name: ");
+        JOptionPane.showMessageDialog(null, userName + "to ty");
+    }
+}
